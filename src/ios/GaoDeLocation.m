c31a09fe3a08be814a5d418ef70de82377f6810e
karoline <xiurong.zhang@china317.com>
2018-04-23 11:00:57 +0800

    高德定位插件

---
+++ src/ios/GaoDeLocation.m
@@ -0,0 +1,28 @@
+/********* GaoDeLocation.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface GaoDeLocation : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation GaoDeLocation
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
